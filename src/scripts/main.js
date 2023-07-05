33fe2e25ffe5b9dbfca6e8bbfc5e172bee69cff5
Mufteev Evgeny <evgenymufteev@gmail.com>
2023-07-05 18:23:11 +0300

    add page index

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,50 @@
 'use strict';
-document.addEventListener('DOMContentLoaded', () => {});
+document.addEventListener('DOMContentLoaded', () => {
+  // модальные окна
+  const handleModalPopup = (btn, blockModal) => {
+    const btns = document.querySelectorAll(btn);
+    const modal = document.querySelector(blockModal);
+    const overlay = document.querySelector('.overlay');
+    const arrCloseButton = document.querySelectorAll('.js-close');
+
+    if (btns && modal) {
+      btns.forEach((btnItem) => {
+        btnItem.addEventListener('click', (evt) => {
+          evt.preventDefault();
+          modal.classList.add('active');
+          overlay.classList.add('active');
+          document.body.classList.add('no-scroll');
+        });
+      });
+
+      arrCloseButton.forEach((closeButton) => {
+        closeButton.addEventListener('click', (evt) => {
+          evt.preventDefault();
+          modal.classList.remove('active');
+          overlay.classList.remove('active');
+          document.body.classList.remove('no-scroll');
+        });
+      });
+
+      overlay.addEventListener('click', (evt) => {
+        evt.preventDefault();
+        modal.classList.remove('active');
+        overlay.classList.remove('active');
+        document.body.classList.remove('no-scroll');
+      });
+
+      if (window.screen.width > 767) {
+        document.addEventListener('keydown', (evt) => {
+          if (evt.key === 'Escape') {
+            evt.preventDefault();
+            modal.classList.remove('active');
+            overlay.classList.remove('active');
+            document.body.classList.remove('no-scroll');
+          }
+        });
+      }
+    }
+  };
+
+  handleModalPopup('.js-qr-btn', '.js-qr-modal');
+});
