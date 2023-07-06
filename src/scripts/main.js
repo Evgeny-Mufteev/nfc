3534df15f7b81d657c1ad1de15bb60d975ebebb9
Mufteev Evgeny <evgenymufteev@gmail.com>
2023-07-06 17:53:31 +0300

    add auth page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -45,6 +45,50 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     }
   };
-
   handleModalPopup('.js-qr-btn', '.js-qr-modal');
+
+  // Показать/Скрыть пароль
+  const togglePassword = () => {
+    const authForm = document.querySelector('.auth__form');
+    if (authForm) {
+      const inputs = authForm.querySelectorAll('.auth__input[type="password"]');
+      const btn = authForm.querySelector('.auth__password-icon');
+
+      const handleBtnClick = (evt) => {
+        evt.preventDefault();
+        inputs.forEach((input) => {
+          input.type = input.type === 'password' ? 'text' : 'password';
+        });
+        btn.classList.toggle('active');
+      };
+
+      btn.addEventListener('click', handleBtnClick);
+    }
+  };
+
+  togglePassword();
+
+  // Валидация и отправка формы НЕ в модальном окне
+  const handleFormSubmitPage = (formItem) => {
+    const form = document.querySelector(formItem);
+
+    if (form) {
+      const pristine = new Pristine(form);
+
+      form.addEventListener('submit', (evt) => {
+        evt.preventDefault();
+        const valid = pristine.validate();
+        if (valid) {
+          evt.preventDefault();
+
+          const formData = Object.fromEntries(new FormData(evt.target).entries());
+          console.log(formData);
+          // evt.target.submit()
+        }
+      });
+    }
+  };
+
+  handleFormSubmitPage('form[name="auth_form"]');
+  handleFormSubmitPage('form[name="registr_form"]');
 });
